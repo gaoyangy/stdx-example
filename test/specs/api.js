78df7ffe23be8d2704c240e8079b91590f9ef3fa
jetiny <86267344@qq.com>
2018-03-23 09:15:04 +0800

    dump node-sass

--- test/specs/api.js
+++ test/specs/api.js
@@ -1,12 +1,11 @@
 import test from 'ava'
 import {expect} from 'chai'
-import {shallow} from 'vue-test-utils'
+import {shallow} from '@vue/test-utils'
 import {ucfirst, lcfirst, log, App} from '../../src'
 
 test('simple', async (t) => {
   expect(ucfirst('ab')).to.eql('Ab')
   expect(lcfirst('CD')).to.eql('cD')
-  log('ab', 'CD')
   const msg = 'new message'
   const wrapper = shallow(App, {
     propsData: { msg }
